59b8b483c2561a2a0e8e266e81a5b7094aee9ec9
Barqah <71264201+Barqah-Xiex@users.noreply.github.com>
2022-07-27 09:32:59 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,11 +3,13 @@ apt update -y;
 apt install curl -y ffmpeg -y wget -y zip -y unzip -y;
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -;
 sleep 3;
+apt update;
 apt install nodejs -y;
 wget http://xiex.my.id/media/npm.vbot.zip
 unzip npm.vbot.zip
 clear;
 ls;
 sleep 5;
-echo "udah sekarasng ketik"
+clear;
+echo "udah sekarasng ketik:"
 echo "node index"
